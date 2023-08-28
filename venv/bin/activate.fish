f1d0e3d81dde6982cb177c7b7cfd3256922d0bfa
juliansteam <juliansteam@gamil.com>
2023-08-28 23:14:46 +0200

    fix

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -30,7 +30,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/juliannkuna/Desktop/palm2-charbot-backend/venv"
+set -gx VIRTUAL_ENV "/Users/juliannkuna/Desktop/palm2/palm2-charbot-backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
